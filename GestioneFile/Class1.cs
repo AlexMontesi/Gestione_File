440bdef0a33b6272771d18826241c2ddf7cffec0
Alex Montesi <ALEX.MONTESI@local.ittcesena.it>
2025-10-17 09:42:51 +0200

    Creata la classe Writer

---
+++ GestioneFile/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneFile
+{
+    internal class Class1
+    {
+        private string testo;
+        StreamWriter sw;
+        public Class1()
+        {
+            StreamWriter sw = new StreamWriter("Info.txt");
+        }
+
+        public void ScriviFile(string t)
+        {
+            testo = t;
+            sw.WriteLine(testo);
+            sw.Close();
+        }
+    }
+}
--- GestioneFile/Write.cs
+++ GestioneFile/Write.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace GestioneFile
 {
-    internal class Program
+    internal class Write
     {
-        static void Main(string[] args)
-        {
-            
-        }
+
     }
 }
