e8c110084f9cf7095d4d56afd23b28d49024c9d3
Alex Montesi <ALEX.MONTESI@local.ittcesena.it>
2025-10-24 09:37:45 +0200

    R0.1:Completata la classe read

--- GestioneFile/Read.cs
+++ GestioneFile/Read.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,30 @@ namespace GestioneFile
 {
     internal class Read
     {
+        private string filePath;
+
+        public Read(string path)
+        {
+            filePath = path;
+        }
+            public void LeggiFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Errore nella lettura del file" + e.Message);
+            }
+         }
     }
 }
