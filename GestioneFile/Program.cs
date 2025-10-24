10c89b2fb4b329776c115afa07418663019b05de
Alex Montesi <ALEX.MONTESI@local.ittcesena.it>
2025-10-24 09:45:05 +0200

    R0.1:Utilizzo della classe write

--- GestioneFile/Program.cs
+++ GestioneFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,14 @@ namespace GestioneFile
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Inserisci il path");
+            string path = Console.ReadLine();
+
+            Write w = new Write(path);
+
+            w.ScriviFile();
+
+            Console.WriteLine("Operazione completata");
         }
     }
 }
